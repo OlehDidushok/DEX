a49a8250ffba359ee4eaa7e35fda66866e03d5e6
Oleh Didushok <didushokov@gmail.com>
2025-06-27 18:17:50 +0300

    Added widget extension

---
+++ DEX/DexWidget/DexWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  DexWidgetBundle.swift
+//  DexWidget
+//
+//  Created by Oleh on 27.06.2025.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct DexWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        DexWidget()
+    }
+}
